37aa972acf09b7c387d0c596137d7bb8cc535d8e
My Name <muzaki.ahmad@ui.ac.id>
2024-09-06 20:21:32 +0700

    `feat: tutorial 1 - convert with view`.

--- romanconverter/src/main/java/com/tutorial/romanconverter/controller/RomanConverterController.java
+++ romanconverter/src/main/java/com/tutorial/romanconverter/controller/RomanConverterController.java
@@ -5,9 +5,12 @@ package com.tutorial.romanconverter.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.tutorial.romanconverter.dto.RequestDTO;
 import com.tutorial.romanconverter.model.RomanConverter;
 
 
@@ -27,6 +30,22 @@ public class RomanConverterController {
     public String romanConverterWithReqParam(@RequestParam(value = "roman") String roman, Model model) {
         return getRomanConverterPage(roman, model);
     }
+    @GetMapping(value = "/convert")
+    public String getRomanCoverterWithView(Model model) {
+        var requestDTO = new RequestDTO();
+        model.addAttribute("requestDTO", requestDTO);
+        return "form.html";
+    }
+
+
+    @PostMapping(value = "/convert")
+    public String postRomanConverterWithView(
+        @ModelAttribute RequestDTO requestDTO, Model model
+    ) {
+        String romanFromView = requestDTO.getRoman();
+        return getRomanConverterPage(romanFromView, model);
+    }
+
 
 private String getRomanConverterPage(String roman, Model model) {
         final RomanConverter romanConverter = new RomanConverter(roman);
