a24948829832beca6fc4442c48ade6dd6dbfac91
My Name <muzaki.ahmad@ui.ac.id>
2024-09-06 20:17:24 +0700

    feat: tutor 1 - add path var

--- romanconverter/src/main/java/com/tutorial/romanconverter/controller/RomanConverterController.java
+++ romanconverter/src/main/java/com/tutorial/romanconverter/controller/RomanConverterController.java
@@ -5,17 +5,30 @@ package com.tutorial.romanconverter.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.tutorial.romanconverter.model.RomanConverter;
 
 
 
 
 @Controller
 public class RomanConverterController {
-
-
     @GetMapping(value = "/")
     public String home(Model model) {
         return "view-home.html";
     }
+    @GetMapping(value = "/roman-converter/{roman}")
+    public String romanConverterWithPathVariable(@PathVariable(value = "roman") String roman, Model model) {
+        return getRomanConverterPage(roman, model);
+    }
+
+private String getRomanConverterPage(String roman, Model model) {
+        final RomanConverter romanConverter = new RomanConverter(roman);
+        model.addAttribute("romanConverter", romanConverter);
+        return "view-conversion-result.html";
+    }
+
 }
+    
 
